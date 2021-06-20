661023c744b3db57f14e93f2eac376c54fb3d9dd
Trac34 <50154763+Trac34@users.noreply.github.com>
2021-06-20 02:58:47 -0400

    Update shellify.sh

--- shellify.sh
+++ shellify.sh
@@ -3,4 +3,7 @@
  if [ $# -ne 1 ]; then exit; fi
  if ! [[ -f $1 ]]; then exit; fi
  objdump -d $1 |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d'' -s - |sed 's/^/"/'|sed 's/$/"/g'
-~                  
+ echo
+ echo "--------------------------------Format------------------------------------"
+ echo
+ for i in $(objdump -d $1 | grep "^ " | cut -f1 | cut -d: -f2); do echo -n "\\x$i"; done
