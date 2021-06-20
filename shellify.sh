4f698d5ceb7f072b6a1fcd94d2ffd2eb7148e9f6
Trac34 <50154763+Trac34@users.noreply.github.com>
2021-06-19 23:15:18 -0400

    Update shellify.sh

--- shellify.sh
+++ shellify.sh
@@ -2,5 +2,5 @@
  
  if [ $# -ne 1 ]; then exit; fi
  if ! [[ -f $1 ]]; then exit; fi
- objdump -d $1|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-7 -d' '|tr -  s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/     '|sed 's/$/"/g'
+ objdump -d $1 |grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '|tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d'' -s - |sed 's/^/"/'|sed 's/$/"/g'
 ~                  
